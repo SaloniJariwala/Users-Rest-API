623612374e51cdb1edbe911a2f6412148d94b7c0
SaloniJariwala <salonijariwala.developing@gmail.com>
2022-04-05 23:42:32 +0530

    Changes in Schema

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
 const connectDB = async () => {
     try {
         const con = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected : ${con.connection.host}`,pink.underline);
+        console.log(`MongoDB Connected : ${con.connection.host}`);
     }
     catch (e) {
         console.log(e);
--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -3,14 +3,21 @@ const asyncHandler = require("express-async-handler")
 const User = require("../Model/userModel");
 
 const getUsers = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Get Users"});
+    const users = await User.find()
+    res.status(200).json(users);
 })
 
 const setUsers = asyncHandler(async (req, res) => {
+    console.log("-----In Set User-----");
     if(!req?.body?.name){
         res.status(400);
         throw new Error("Please Provide Your Name");
     }
+
+    const user = await User.create({
+        name: req.body.name
+    })
+    res.status(2000).json(user);
 })
 
 const putUsers = asyncHandler(async (req, res) => {
