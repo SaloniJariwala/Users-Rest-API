87e53791f8c0b6611a978a6d791c733eefb3b5ec
Parth Modi <parthmodi.developer@gmail.com>
2022-04-06 15:44:59 +0530

    Rest API for Users

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -3,28 +3,49 @@ const asyncHandler = require("express-async-handler")
 const User = require("../Model/userModel");
 
 const getUsers = asyncHandler(async (req, res) => {
-    const users = await User.find()
+    const users = await User.find();
     res.status(200).json(users);
 })
 
 const setUsers = asyncHandler(async (req, res) => {
     console.log("-----In Set User-----");
     if(!req?.body?.name){
+        //console.log(req.body.name);
         res.status(400);
         throw new Error("Please Provide Your Name");
     }
-
     const user = await User.create({
         name: req.body.name
-    })
-    res.status(2000).json(user);
+    });
+    //console.log(user);
+    res.status(200).json(user);
 })
 
 const putUsers = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update user having id ${req.params.id}`});
+    const user = await User.findById(req.params.id);
+
+    if(!user){
+        res.status(400);
+        throw new Error("User Not Found")
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedUser);
 })
 
 const deleteUsers = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if(!user){
+        res.status(400);
+        throw new Error("User Not Found")
+    }
+
+    await user.remove();
+
     res.status(200).json({ message: `Delete user having id ${req.params.id}`});
 })
 
